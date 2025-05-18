223c618d942ae84ecac87a653fde72336dd21215
urkiddennn <154726035+urkiddennn@users.noreply.github.com>
2025-05-18 14:50:57 +0800

    Update: Uses Gorilla/mux in server

--- web-server/main.go
+++ web-server/main.go
@@ -3,17 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-	})
+	r := mux.NewRouter()
 
-	fs := http.FileServer(http.Dir("static/"))
+	r.HandleFunc("/book/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
+	//	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	//	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	//})
+
+	// fs := http.FileServer(http.Dir("static/"))
 
 	// gandle file server
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	//	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":80", r)
 }
