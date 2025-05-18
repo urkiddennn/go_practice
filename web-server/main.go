bba388aacb481327b4c479d1441fd472f32f759e
urkiddennn <154726035+urkiddennn@users.noreply.github.com>
2025-05-18 14:36:30 +0800

    Update: Practice file server in Go!

--- web-server/main.go
+++ web-server/main.go
@@ -10,5 +10,10 @@ func main() {
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
 
+	fs := http.FileServer(http.Dir("static/"))
+
+	// gandle file server
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	http.ListenAndServe(":80", nil)
 }
